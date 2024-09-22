1964e89d5fc3944cdf286ea6d309c0f3870e63db
CHZelaya <chzelaya1201@gmail.com>
2024-09-21 23:31:30 -0600

    final commit for the night, working on logic for opening/closing a new window tomorrow

--- js/index.js
+++ js/index.js
@@ -1,8 +1,8 @@
-// 
+//
 
 
-let pictureContainer = document.querySelector('.picture-container')
-let imgArray = [
+const pictureContainer = document.querySelector('.picture-container')
+const imgArray = [
     {
         src: '../images/banff-bridge.jpg'
     },
@@ -17,24 +17,45 @@ let imgArray = [
     }
 ]
 
-let descriptionArray = [
+
+const descriptionArray = [
     "Banff Bridge seen in Summer",
     "Fairmont Hotel seen from the lookout",
     "The RimRock Hotel seen from one of its balconies",
     "Moraine Lake shown with its canoe's",
 ];
-let loadPictures = () => {
 
-    console.log('function is being called')
+const urlArray = [
+    'https://banff.ca/',
+    'https://www.fairmont.com/banff-springs',
+    'https://www.rimrockresort.com/',
+    'https://www.banfflakelouise.com/',
+]
+
+const loadPictures = () => {
+
     imgArray.forEach((picture, index) => {
         // ? Note to self: you need an extra set of () around the parameters when you're using multiple parameters
-        let img = document.createElement('img'); //* Creating an img element
-        img.src = picture.src //* assigning the source of the image from the imgArray
-        img.classList.add('img') //* adding a class for styling purposes
-        pictureContainer.appendChild(img); //* appending the child to the already existing div.
+
+        // * Creating a-href element
+        let anchor = document.createElement('a');
+        // * Setting href as url in urlArray
+        anchor.href = urlArray[index];
+        // * <a-href> to parent <div>
+        pictureContainer.appendChild(anchor);
+
+        // * Creating <img> element
+        let img = document.createElement('img');
+        // * Setting <img src=''> from imgArray
+        img.src = picture.src
+        // * Adding class for styling and appending img to <a-href>
+        img.classList.add('img')
+        anchor.appendChild(img);
 
 
-        // * Handling the image descriptions, and again adding and removing classes that provide styling while being triggered by mouseover and mouseleave
+
+
+        // * Handling the image indexriptions, and again adding and removing classes that provide styling while being triggered by mouseover and mouseleave
         let description = document.createElement('p');
         description.textContent = descriptionArray[index];
         description.classList.add('hidden')
@@ -50,6 +71,10 @@ let loadPictures = () => {
             description.classList.remove('shown');
             description.classList.add('hidden');
         })
+
+        img.addEventListener('click', () => {
+
+        })
     })
 };
 
@@ -59,3 +84,35 @@ let loadPictures = () => {
 document.addEventListener('DOMContentLoaded', loadPictures())
 // ? I didn't know you could do this, good ol' MDN.
 // ? Link: https://developer.mozilla.org/en-US/docs/Web/API/Document/DOMContentLoaded_event
+
+
+// ! Testing setTimeout() and how it works below
+
+let openWindow;
+let url = 'http://www.google.ca'
+
+const openCloseWindow = (url) => {
+
+
+    setTimeout(() => {
+        openWindow = window.open(url);
+
+        if (openWindow) {
+            console.log('Window opened successfully')
+
+            setTimeout(() => {
+                return closeWindow(url);
+
+            }, 5000)
+        }
+    }, 1000);
+
+}
+
+const closeWindow = (url) => {
+    setTimeout(() => {
+        console.log('window close attempted')
+        openWindow = window.close(url);
+    }, 5000)
+}
+document.querySelector('.test').addEventListener('click', openCloseWindow)
